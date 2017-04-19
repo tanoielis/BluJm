216cc9832eeafe3513ac5e971ad1ef7b66cb787d
tanoielis <tanoielis@users.noreply.github.com>
2017-04-19 17:01:56 +1200

    Fixed Collision lib Made example

--- src/com/the_dungeoneers/game/Game.java
+++ src/com/the_dungeoneers/game/Game.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public class Game extends PApplet{
 
-	int tick = 0;
 	boolean intersection = false;
 
 	List<Polygon> polygons = new ArrayList<>();
@@ -30,18 +29,17 @@ public class Game extends PApplet{
 		drawPolygons();
 	}
 	private void createPolygons() {
-		polygons.add(new Square(500,650, 50, 50, color(255,0,0), 0));
-		polygons.add(new Square(600,600, 100, 100, color(0,255,0), 0));
+		polygons.add(new Square(this, 500,650, 50, 50, color(255,0,0), 0));
+		polygons.add(new Square(this, 590,600, 100, 100, color(0,255,0), 70));
 
-		polygons.add(new Triangle(700,650, 800, 650, (700 + 800)/2, 550, color(255,0,0), 0));
+		polygons.add(new Triangle(this, 700,650, 800, 650, (700 + 800)/2, 550, color(255,0,0), 0));
 	}
 
 	private void drawPolygons() {
-		tick += 0.01;
 		for (Polygon poly : polygons) {
 			for (Polygon polygon1 : polygons) {
 				if (SAT_Collision.intersects(poly, polygon1)) {
-					if (intersection == false) {
+					if (!intersection) {
 						intersection = true;
 						System.out.println("Intersection detected");
 					}
