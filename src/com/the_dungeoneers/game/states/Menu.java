9cde7814d0909a654d23e2ac5a6a0cee88467f09
Chris and Eli <eli.tanoi@gmail.com>
2017-04-21 19:04:39 +1200

    Added sound lib and font

---
+++ src/com/the_dungeoneers/game/states/Menu.java
@@ -0,0 +1,39 @@
+package com.the_dungeoneers.game.states;
+
+/**
+ * Created by Eli on 21/04/2017.
+ */
+public class Menu implements State{
+	
+	
+	
+	@Override
+	public void update(){
+		
+	}
+	
+	@Override
+	public void drawUI(){
+		
+	}
+	
+	@Override
+	public void drawEntities(){
+		
+	}
+	
+	@Override
+	public void keyPressed(){
+		
+	}
+	
+	@Override
+	public void keyReleased(){
+		
+	}
+	
+	@Override
+	public void mousePressed(){
+		
+	}
+}
